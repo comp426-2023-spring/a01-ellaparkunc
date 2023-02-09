5ee166604d2d7d02b2c649c5a39e1ca29f7ef3d6
Ella Parker <ellapark@live.unc.edu>
2023-02-09 11:33:27 -0500

    script accepts command line arg to set port and reverts to default if none provided

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//#!/usr/bin/env node
+#!/usr/bin/env node
 // Require http module
 const http = require('node:http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+//const port = 3000;
 
 // Require fs module
 const fs = require('fs');
@@ -28,12 +28,14 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 
   
 // Require minimist module (make sure you install this one via npm).
-
+var argv = require('minimist')(process.argv.slice(2));
+console.log(argv);
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
+//done
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
+const port = argv.port || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
